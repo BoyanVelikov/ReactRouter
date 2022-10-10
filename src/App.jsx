29fdb96d4d192c75f9681b5b6dbc5ee1510f759d
Boyan Velikov <boyanvelikov@gmail.com>
2022-10-10 06:11:30 +0000

    10-10-2022

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Link, Route, Routes } from 'react-router-dom'
 import './App.css'
 import {Home} from './pages/Home';
 import {BookList} from './pages/BookList';
+import {Book} from './pages/Book';
 
 export default function App() {
   return (
@@ -18,7 +19,8 @@ export default function App() {
    </nav>
   <Routes>
     <Route path='/' element={<Home />}/>
-    <Route path='books' element={<BookList />}/>
+    <Route path='/books' element={<BookList />}/>
+    <Route path='/books/:id' element={<Book />}/>
   </Routes>
  </>
     
